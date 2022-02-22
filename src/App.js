5593fb0d2d004da148918a3dddfc0cf7fdde391f
KINGSLEYEHAPA <kessity09@gmail.com>
2022-02-22 19:47:06 +0100

    Project structure setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import "./App.css";
-
 function App() {
-  return <div className="App">This is the App Page</div>;
+  return <div className="text-red-500">This is the App Page</div>;
 }
 
 export default App;
