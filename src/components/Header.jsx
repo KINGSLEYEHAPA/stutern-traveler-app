e1bcbb3732c21d92594d2ae274881f67e23f9e15
KINGSLEYEHAPA <kessity09@gmail.com>
2022-02-23 14:05:57 +0100

    Working on UI-Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,53 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Search from "./Search";
+import { RiMenu5Fill } from "react-icons/ri";
 
 const Header = () => {
-  const [isMobile, setIsmobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(true);
   return (
-    <div className="text-red-800 items-center h-16 flex justify-between w-screen px-6">
+    <div className="text-red-800 items-center h-24 relative  flex justify-between w-screen px-6">
       <div>
         <Link
           to="/"
-          className="text-xs sm:text-md md:text-lg lg:text-xl text-white  "
+          className="text-sm sm:text-md md:text-lg lg:text-xl text-white  pr-3 "
         >
-          The Traveller
+          Traveler
         </Link>
       </div>
+
+      <div className=" gap-6 hidden ssm:flex">
+        <p>Home</p>
+        <p>Attractions</p>
+        <p>Locations</p>
+      </div>
+
+      {isMobile && (
+        <div
+          className={
+            isMobile
+              ? "absolute top-24 w-full right-4 px-10 py-5 z-1000 text-center space-y-2 bg-orange-600 ssm transition-all:hidden"
+              : "hidden"
+          }
+        >
+          <p>Home</p>
+          <p>Attractions</p>
+          <p>Location</p>
+        </div>
+      )}
       <Search />
-      <div className=""> </div>
+      <div className="w-36 h-20  text-center py-2 ml-3">
+        <img className="w-6 h-6 rounded-full bg-white mx-auto" src="" alt="" />
+        <h3 className="text-xs pt-2 text-white">Welcome Kingsley</h3>
+      </div>
+      <span
+        onClick={() => {
+          setIsMobile(!isMobile);
+        }}
+        className="text-white ml-2 md:text-2xl "
+      >
+        <RiMenu5Fill />
+      </span>
     </div>
   );
 };
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { RiSearchLine } from "react-icons/ri";
 
 const Search = () => {
   return (
-    <div>
+    <div className="relative ">
       {" "}
       <input
-        className="text-md outline-none rounded-lg text-white bg-sky-400/20 px-4 sm:w-96 py-1 placeholder:text-sm placeholder:text-blue-400"
+        className="text-md md:text-xl outline-none rounded-lg w-44 text-orange-600 bg-sky-400/20 px-4 pr-7 sm:w-44 md:w-[22rem] ssm:translate-x-8 md:h-12 py-1  md:px-6 placeholder:text-sm md:placeholder:text-xl placeholder:text-blue-400"
         placeholder="Search for a location..."
       />
+      <span className="absolute right-2 top-2.5 text-sm text-orange-600 md:top-3 md:text-xl md:right-3 ssm:translate-x-8">
+        <RiSearchLine />
+      </span>
     </div>
   );
 };
